46ad3b4a9f7f06db1d69044ae700990f369e6eaf
Jonathan Balsano <jonathan.balsano@10gen.com>
2017-02-09 15:43:31 -0500

    Speed and efficiency improvements

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ const urlHelper = require('url');
 
 // const
 const passed = {};
-const fakeNewsSites = [ 'http://occupydemocrats.com' ];
-const queue = fakeNewsSites.map(url => { return { url, path: [] }; });
-const AD_INDICATORS = ['ads by', 'advertisement'];
+const fakeNewsSites = [ 'http://occupydemocrats.com/2016/08/19/donald-trumps-campaign-goes-full-racist-calls-obama-half-breed-negro-2/', 'http://occupydemocrats.com/' ];
+let stack = fakeNewsSites.map(url => { return { url, path: [] }; });
+const sponsors = {};
+const adSites = {};
+const AD_INDICATORS = ['ads by', 'advertisement', 'sponsored links'];
+const LIKELY_IFRAMES = [
+    /facebook\.com$/,
+    /youtube\.com$/,
+    /instagram\.com$/,
+    /twitter\.com$/
+];
+const SKIP_HOSTNAMES = [
+    /adblade\.com$/
+];
 
 function domainFilter(url) {
     const baseUrlParts = urlHelper.parse(options.url);
@@ -28,25 +39,47 @@ function urlFilter(url) {
     return domainFilter(url);
 }
 
-
-const processNextInQueue = () => {
-    if (!queue.length) {
+function processNextInStack() {
+    if (!stack.length) {
         nightmare.then(); // ensure nightmare acts on last action in `after`, if any
+        nightmare.end();
         return;
     }
 
-    const { url, path } = queue.shift();
+    const { url, path } = stack.shift();
     const hostname = urlHelper.parse(url).hostname;
 
-    if (!url || passed[url]) {
+    if (!url || passed[url] || path.length > 2) {
+        processNextInStack();
         return;
     }
 
+    if (SKIP_HOSTNAMES.reduce((found, iframe) => found || iframe.test(hostname), false)) {
+        processNextInStack();
+        return;
+    }
+
+    if (path.length > 2) {
+        const uniqueHostnamesInPath = Object.keys(path.reduce((checkedHostnames, currentPath) => {
+            const newHostname = urlHelper.prase(currentPath).hostname;
+            if (!newHostname) {
+                return;
+            }
+            checkedHostnames[newHostname] = true;
+        }, {})).length;
+
+        if (uniqueHostnamesInPath > 2) {
+            return;
+        }
+    }
+
+    console.log('Sponsors', Object.keys(sponsors));
+    console.log(`Stack ${stack.length} total`, stack.map(obj => obj.url).slice(0,10));
     passed[url] = true;
+    adSites[hostname] = true;
 
     nightmare
         .goto(url)
-        .wait(1000)
         .evaluate((AD_INDICATORS, hostname) => {
             function findElsWithText(textOptions) {
                 let elements = [].concat(Array.from(document.body.children));
@@ -101,10 +134,81 @@ const processNextInQueue = () => {
 
             return { adUrls, siteUrls, sponsorUrls };
         }, AD_INDICATORS, hostname)
-        .then((results) => {
-            console.log(results);
-            processNextInQueue();
+        .then(({ adUrls, siteUrls, sponsorUrls }) => {
+            const newPath = path.concat(url);
+            stack = adUrls.map(newUrl => {
+                    return {
+                        url: newUrl,
+                        path: newPath
+                    };
+                })
+                .concat(siteUrls.map(newUrl => {
+                    return {
+                        url: newUrl,
+                        path: newPath
+                    };
+                }))
+                .concat(stack);
+
+            let promiseChain;
+            sponsorUrls.forEach((url) => {
+                const currentHostname = urlHelper.parse(url).hostname;
+                if (LIKELY_IFRAMES.reduce((found, iframe) => found || iframe.test(currentHostname), false)) {
+                    return;
+                }
+
+                if (adSites[currentHostname]) {
+                    return;
+                }
+
+                function checkThisSponsor() {
+                    if (sponsors[currentHostname]) {
+                        const alreadyFoundFromThisSite = sponsors[currentHostname].reduce((alreadyFound, pathToSponsor) => {
+                            return alreadyFound || pathToSponsor[0] === newPath[0];
+                        }, false);
+
+                        if (alreadyFoundFromThisSite) {
+                            return;
+                        }
+                    }
+
+                    console.log('awaiting sponsor', urlHelper.parse(url).hostname);
+                    return nightmare
+                        .goto(url)
+                        .url()
+                        .then(sponsorUrl => {
+                            const sponsorHostname = urlHelper.parse(sponsorUrl).hostname;
+                            console.log('checking sponsor', sponsorHostname);
+                            if (adSites[sponsorHostname]) {
+                                return;
+                            }
+
+                            console.log('saving sponsor', sponsorHostname);
+                            if (sponsors[sponsorHostname]) {
+                                sponsors[sponsorHostname].concat(newPath);
+                            } else {
+                                sponsors[sponsorHostname] = [newPath];
+                            }
+                        }).catch(error => {
+                            console.error('Sponsor search failed:', error);
+                            processNextInStack();
+                        });
+                }
+                if (promiseChain) {
+                    promiseChain = promiseChain.then(checkThisSponsor);
+                } else {
+                    promiseChain = checkThisSponsor();
+                }
+            });
+
+            if (promiseChain) {
+                promiseChain.then(processNextInStack);
+            } else {
+                processNextInStack();
+            }
+        }).catch(function (error) {
+            console.error('Search failed:', error);
         });
 }
 
-processNextInQueue();
+processNextInStack();
